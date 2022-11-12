3eac235f0242d180875fed064048423f170bb47e
raja-2604 <raja.rathinam@flyerssoft.com>
2022-11-12 19:27:21 +0530

    commit changes

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+const App = () => {
+   
+  return (
+    <div>
+        Testing
+      This was made from Scratch! Also First React App!
+    </div>
+  );
+};
+
+
+export default App;
